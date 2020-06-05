3e7c250020855950fb3d5b27f6f9bee332a16a26
cby <cby@cby-VirtualBox>
2020-06-05 09:57:55 -0500

    Section 33 Linear Regression Models - Complete

---
+++ src/main/java/com/leseanbruneau/GymComptitors.java
@@ -0,0 +1,56 @@
+package com.leseanbruneau;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.ml.feature.VectorAssembler;
+import org.apache.spark.ml.regression.LinearRegression;
+import org.apache.spark.ml.regression.LinearRegressionModel;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+public class GymComptitors {
+
+	public static void main(String[] args) {
+		
+		// To set Windows Environment variable
+		//System.setProperty("hadoop.home.dir", "c:/hadoop");		
+		//Logger.getLogger("org.apache").setLevel(Level.WARN);
+		
+		// Note: Windows need to add .config() for directory, windows directory does not have to exist to run
+		//SparkSession spark = SparkSession.builder().appName("testingSql").master("local[*]")
+		//		.config("spark.sql.warehouse.dir","file:///c:/temp/spark")
+		//		.getOrCreate();
+		
+		// Run on Linux		
+		Logger.getLogger("org.apache").setLevel(Level.WARN);
+		
+		SparkSession spark = SparkSession.builder().appName("Gym Competitors").master("local[*]")
+				.getOrCreate();
+		
+		Dataset<Row> csvData = spark.read()
+				.option("header", true)
+				.option("inferSchema", true)
+				.csv("src/main/resources/GymCompetition.csv");
+		
+		//csvData.show();
+		csvData.printSchema();
+		
+		VectorAssembler vectorAssembler = new VectorAssembler();
+		
+		vectorAssembler.setInputCols(new String[] {"Age","Height","Weight"});
+		vectorAssembler.setOutputCol("features");
+		Dataset<Row> csvDataWithFeatures = vectorAssembler.transform(csvData);
+
+		//csvDataWithFeatures.show();
+		Dataset<Row> modelInputData = csvDataWithFeatures.select("NoOfReps","features").withColumnRenamed("NoOfReps", "label");
+		modelInputData.show();
+		
+		LinearRegression linearRegression = new LinearRegression();
+		LinearRegressionModel model = linearRegression.fit(modelInputData);
+		System.out.println("The model has intercept " + model.intercept() + " and coefficients " + model.coefficients());
+		
+		model.transform(modelInputData).show();
+	}
+
+}
